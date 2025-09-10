4cda1338be721ae6f37d99d6bd28beab7a4b4e20
BiaginiMatheus <matheus.biagini@hotmail.com>
2025-09-10 18:21:02 +0200

    Adding comments for clarity

--- sketch.js
+++ sketch.js
@@ -2,7 +2,8 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
 }
 
-let time = 0; //time is theta (angle)
+//time is theta (angle)
+let time = 0;
 let wave = [];
 
 const circleBaseRadius = 150;
@@ -16,33 +17,48 @@ function draw() {
     let x = 0;
     let y= 0;
     for (let i = 0; i < numberOfCircles; i++) {
+        // Save the previous position
         let previousX = x;
         let previousY = y;
-        let n = i * 2 + 1; //n is the number of circles
+
+        // n is the harmonic number (1, 3, 5, 7, ...)
+        let n = i * 2 + 1;
         let radius = circleBaseRadius * (4 / (n * PI));
         x += radius * cos(n * time);
         y += radius * sin(n * time);
 
+        // Draw the circles
         stroke(255, 100);
         noFill();
         circle(previousX, previousY, radius * 2);
 
+        // Draw the radius line from the center of the circle
+        // to the center of the next circle
         fill(255);
         stroke(255);
         line(previousX, previousY, x, y);
         circle(x, y, dotRadius);
 
     }
+    // Add the y value of the last circle to the wave array
+    wave.unshift(y);
+
+    // Line from the last circle to the wave
     translate(200, 0);
     line(x - 200, y, 0, wave[0]);
-    wave.unshift(y);
+
+    // Draw the wave
     beginShape();
     noFill();
     for (let i = 0; i < wave.length; i++) {
         vertex(i, wave[i]);
     }
     endShape();
+
+    // Increment time/angle for rotation
     time += 0.02;
+
+    // Limit the length of the wave array
     if (wave.length > width / 2 - 200) {
         wave.pop();
     }
